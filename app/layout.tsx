c5e6c82dae162045258b6b45aa22a97f0b12a074
Yatharth786 <yatharth@afidigitalservices.com>
2026-06-09 09:51:01 +0530

    revert the changes in layout file in app for google analytics tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
       <head>
         <meta name="google-site-verification" content="HjZKe36xF3DWrHf2_ac6h3DE-XutF5Oim9bT2Y3o4Z4" />
         
-        {/* Google Tag Manager - temporarily disabled */}
-        {/* <Script id="google-tag-manager" strategy="afterInteractive">
+        {/* Google Tag Manager*/}
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -29,11 +29,11 @@ export default function RootLayout({
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-MT94TTKS');
           `}
-        </Script> */}
+        </Script>
         {/* End Google Tag Manager */}
 
-        {/* Google tag (gtag.js) - temporarily disabled */}
-        {/* <Script
+        {/* Google tag (gtag.js)*/}
+        <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-QK3Z686F0N"
           strategy="afterInteractive"
@@ -45,18 +45,18 @@ export default function RootLayout({
             gtag('js', new Date());
             gtag('config', 'G-QK3Z686F0N');
           `}
-        </Script> */}
+        </Script>
       </head>
       <body>
-        {/* Google Tag Manager (noscript) - temporarily disabled */}
-        {/* <noscript>
+        {/* Google Tag Manager (noscript)*/}
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-MT94TTKS"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
-        </noscript> */}
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
 
         <Providers>
