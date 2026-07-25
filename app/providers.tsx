7335287ac948234bb77c66882520fa33934527b0
Yatharth786 <yatharth@afidigitalservices.com>
2026-07-25 10:41:46 +0530

    wrapped the I18nClientProvider in provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { TooltipProvider } from "@/components/ui/tooltip";
-
 import { ThemeProvider } from "@/components/theme-provider";
+import { I18nClientProvider } from "@/lib/i18n-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <TooltipProvider delayDuration={300}>
-        {children}
-      </TooltipProvider>
+      <I18nClientProvider>
+        <TooltipProvider delayDuration={300}>
+          {children}
+        </TooltipProvider>
+      </I18nClientProvider>
     </ThemeProvider>
   );
 }
